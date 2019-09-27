be7d84b7fc9e1391c05cc6cb781486e9e115c2f0
nikola_w <n@s>
2019-09-27 19:55:34 +0200

    Completed checks for backups with given prob.

--- Backup_cost.py
+++ Backup_cost.py
@@ -1,5 +1,5 @@
 import numpy as np
-import datetime as dt
+# import datetime as dt
 
 
 class Backup_cost:
@@ -19,20 +19,30 @@ class Backup_cost:
         if success == 1:
             return [success, backup_date]
         else:
-            return success
+            return [success, "nada"]
 
     def data_loss(self):
         success = 0
-        successful_backups = []
+        number_of_backups = len(self.backups)
+        print("Number of backups:", number_of_backups)
+        # successful_backups = []
+        i = 0
         while success == 0:
-            i = 0
             info = self.recovery_attempt(self.backups[i])
             success = info[0]
+            print("tried with backup date:", self.backups[i])
+            print("success state:", info[0])
+            print("successful backup date:", info[1])
+            if i == number_of_backups-1 and success == 0:
+                print("Couldn't restore data")
+                break
             i += 1
-        successful_backups.append(info[1])
+        # successful_backups.append(info[1])
 
     def backup_price_total(self):
         self.initial_price() + self.data_loss()*self.work_rate
 
-backups = ['8/26/2019', '5/26/2019', '2/26/2019']
-Backup_cost(300, 30, '9/26/2019', )
+
+backs = ['8/26/2019', '5/26/2019', '2/26/2019']
+a = Backup_cost(300, 30, '9/26/2019', backs, 6/7)
+a.data_loss()

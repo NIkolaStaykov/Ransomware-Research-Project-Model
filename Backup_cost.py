f9a95236a24c0bd6f8c5a930d9a8c38f4f411b51
nikola_staykov <nikola@st>
2019-10-18 20:34:44 +0300

    Trying to ship the slider file with the random generator file. Still not ready.

--- Backup_cost.py
+++ Backup_cost.py
@@ -6,17 +6,12 @@ import matplotlib.pyplot as plt
 class Backup_cost:
     info = []
 
-    def __init__(self, work_rate, recovery_price, disaster_date, backups, fail_prob):
+    def __init__(self, work_rate, single_try_price, disaster_date, backups, fail_prob):
         self.work_rate = work_rate
-        self.recovery_price = recovery_price
         self.disaster_date = datetime.datetime.strptime(disaster_date, '%m/%d/%Y')
         self.backups = backups
         self.prob = fail_prob
-
-    def initial_price(self):
-        delta_t = 1
-        in_price = self.recovery_price + delta_t*self.work_rate
-        return in_price
+        self.single_try_price = single_try_price
 
     def recovery_attempt(self, backup_date):
         success = np.random.choice([1, 0], 1, p=[1 - self.prob, self.prob])
@@ -26,55 +21,61 @@ class Backup_cost:
             return [success, "nada"]
 
     def data_loss(self):
-        global info  # info[0] is the success state of the recovery, info[1] is the successful backup date
-        success = 0
+        global info  # info[0] is the success state of the recovery, info[1] is the successful backup date or "nada"
         number_of_backups = len(self.backups)
         # print("Number of backups:", number_of_backups)
         # successful_backups = []
         i = 0
+        success = 0
         while success == 0:
-            info = self.recovery_attempt(self.backups[i])
-            success = info[0]
-            # print("tried with backup date:", self.backups[i])
-            # print("success state:", info[0])
-            print("successful backup date:", info[1])
-            if i == number_of_backups-1 and success == 0:
+            if datetime.datetime.strptime(self.backups[i], '%m/%d/%Y') < self.disaster_date:
+                info = self.recovery_attempt(self.backups[i])
+                success = info[0]
+                print("tried with backup date:", self.backups[i])
+                print("success state:", success)
+            else:
+                print(self.backups[i], "does not exist yet")
+            if i == number_of_backups - 1 and success == 0:
                 print("Couldn't restore data")
-                info[1] = 0
                 break
             i += 1
-        # successful_backups.append(info[1])
-        if info[1] == 0:
-            unsuccessful_recovery()
+        info.append(i)
         return info
 
-    def time_delta(self):
-        state = self.data_loss()
-        if state[1] != 0:
-            successful_backup_date = datetime.datetime.strptime(state[1], '%m/%d/%Y')
-            time_passed = self.disaster_date-successful_backup_date
-            return time_passed
+    def time_delta(self, recovery_date):
+        recovery_date = datetime.datetime.strptime(recovery_date, '%m/%d/%Y')
+        time_passed = self.disaster_date - recovery_date
+        return time_passed
 
     def backup_price_total(self):
-        data_delta = self.time_delta()
-        total = self.initial_price() + data_delta.days*self.work_rate
-        # print("total recovery price:", total)
-        return total
+        new_info = self.data_loss()
+        if new_info[0] == 1:
+            backup_delta = self.time_delta(new_info[1])
+            total = backup_delta.days*self.work_rate + info[2] * self.single_try_price
+            print("total recovery price:", total)
+            return total
+        if new_info[0] == 0:
+            print("Pay the ransom if data value is less")
+            return None
 
-
-def unsuccessful_recovery():
-    print("You better pay the ransom")
+    def point(self):
+        x = self.time_delta(self.backups[-1])
+        y = self.backup_price_total()
+        return [x, y]
 
 
 backs = ['9/23/2019', '9/20/2019', '9/13/2019']
+
 # Backup_cost(work rate, recovery try price, disaster date, backup dates, failure probability)
-a = Backup_cost(300, 30, '09/26/2019', backs, 1/5)
+a = Backup_cost(300, 50, '09/25/2019', backs, 1/2)
+a.backup_price_total()
 x = []
 y = []
-for i in range(20):
+for it in range(200):
+    # for k in range(5):
     y.append(a.backup_price_total())
     x.append(a.disaster_date)
     a.disaster_date += datetime.timedelta(days=1)
-print(y)
 plt.plot(x, y, color="red")
+plt.gcf().autofmt_xdate()
 plt.show()
--- Slider_plot_0.py
+++ Slider_plot_0.py
@@ -1,8 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
+import datetime
+import Backup_cost
 
+# take user input for parameters needed
 days = int(input("Number of days to visualize:"))
+initial_work_rate = int(input("Initial work rate:"))
+init_price_big = int(input("Initial price for a recovery try of full backups:"))
+init_price_small = int(input("Initial price for a recovery try of incremental backups:"))
+
+
+# setup backup dates arrays as diff from initial time point
 
 backup_dates_big = []
 for i in range(days):
@@ -16,14 +25,12 @@ for i in range(days):
 
 backup_dates = backup_dates_big + backup_dates_small
 
-initial_work_rate = int(input("Initial work rate:"))
-init_price_big = int(input("Initial price for a recovery try of full backups:"))
-init_price_small = int(input("Initial price for a recovery try of incremental backups:"))
 
-fig, ax = plt.subplots()
-plt.subplots_adjust(left=0.25, bottom=0.25)
-
-dates = np.arange(0.0, days, 1)
+dates_numbers = np.arange(0.0, days, 1)
+dates_datetime = []
+last_backup_date = '10/18/2019'
+for i in dates_numbers:
+    dates_datetime.append(datetime.datetime.strptime(last_backup_date, '%m/%d/%Y') + datetime.timedelta(days=i))
 counter = 0
 fail_big = 0
 fail_small = 0
@@ -33,39 +40,62 @@ def y_data(single_try_big, single_try_small,  w_rate):
     new_counter = 0
     new_fail_big = 0
     new_fail_small = 0
-    prices_new = dates.copy()
-    for new_time_iterator in dates:
+    prices_high = dates_numbers.copy()
+    for new_time_iterator in dates_numbers:
         for new_backup_date_iterator in backup_dates_big:
             if new_time_iterator == new_backup_date_iterator:
                 new_fail_big += 1
         for new_backup_date_iterator in backup_dates_small:
             if new_time_iterator == new_backup_date_iterator:
                 new_fail_small += 1
-        prices_new[new_counter] = dates[new_counter] * w_rate + single_try_big * new_fail_big + single_try_small * new_fail_small
+        prices_high[new_counter] = dates_numbers[new_counter] * w_rate + single_try_big * new_fail_big + single_try_small * new_fail_small
         new_counter += 1
-    return prices_new
+    return prices_high
 
 
 def y_data_1(w_rate):
     new_counter = 0
-    prices_new = dates.copy()
+    prices_low = dates_numbers.copy()
     last_backup = 0
-    for time_iterator in dates:
+    for time_iterator in dates_numbers:
         for backup_date_iterator in backup_dates:
             if time_iterator == backup_date_iterator:
                 last_backup = backup_date_iterator
-        prices_new[new_counter] = (dates[new_counter] - last_backup) * w_rate
+        prices_low[new_counter] = (dates_numbers[new_counter] - last_backup) * w_rate
         new_counter += 1
-    return prices_new
+    return prices_low
 
 
-prices_1 = y_data_1(initial_work_rate)
+a = Backup_cost.Backup_cost(initial_work_rate, init_price_small, None, None, 1/2)
+a.disaster_date = '01/10/2019'
+for i in backup_dates:
+    dates_datetime.append(datetime.datetime.strptime(last_backup_date, '%m/%d/%Y') + datetime.timedelta(days=i))
 
-k, = plt.plot(dates, prices_1, color='red', lw=1.2)
+
+def data_random(point_generator, points_count):
+    y_data_rand = []
+    x_data_rand = []
+    for n in range(points_count):
+        point = point_generator.point()
+        y_data_rand.append(point[1])
+        x_data_rand.append(point[0])
+    return [x_data_rand, y_data_rand]
+
+
+fig, ax = plt.subplots()
+plt.subplots_adjust(left=0.15, bottom=0.3)
 
 prices = y_data(init_price_big, init_price_small, initial_work_rate)
+l, = plt.plot(dates_numbers, prices, lw=1.2)
+plt.xlabel("Days from first backup")
+plt.ylabel("Backup price")
 
-l, = plt.plot(dates, prices, lw=1.2)
+prices_1 = y_data_1(initial_work_rate)
+k, = plt.plot(dates_numbers, prices_1, color='red', lw=1.2)
+
+random_prices = data_random(a, 180)[1]
+random
+m, = plt.scatter(dates_numbers, random_prices, color='green', lw=1.2)
 ax.margins(x=0)
 
 ax_color = 'lightgoldenrodyellow'

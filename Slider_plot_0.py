1016fdee55f30e3ec81deebccf5ba50063f3b0a4
nikola_staykov <nikola@st>
2019-11-14 00:06:47 +0200

    Almost done with everything, only the beginning of the red thingy (lower boundary) is off now.

--- Slider_plot_0.py
+++ Slider_plot_0.py
@@ -43,32 +43,38 @@ for i in dates_numbers:
 
 
 # y data functions for the two linear graphics
-def y_data(single_try_big, single_try_small,  w_rate):
+def y_data_high(single_try_big, single_try_small, w_rate):
     new_counter = 0
     new_fail_big = 0
     new_fail_small = 0
     prices_high = dates_numbers.copy()
-    for new_time_iterator in dates_numbers:
-        for new_backup_date_iterator in backups_big:
-            if new_time_iterator == new_backup_date_iterator:
-                new_fail_big += 1
-        for new_backup_date_iterator in backups_small:
-            if new_time_iterator == new_backup_date_iterator:
-                new_fail_small += 1
+    for new_time_iterator in dates_datetime:
+        for new_backup_date_iterator in backups:
+            if new_time_iterator == new_backup_date_iterator.date:
+                if new_backup_date_iterator.backup_type == 'small':
+                    new_fail_small += 1
+                if new_backup_date_iterator.backup_type == 'big':
+                    new_fail_big += 1
         prices_high[new_counter] = dates_numbers[new_counter] * w_rate + single_try_big * new_fail_big + single_try_small * new_fail_small
         new_counter += 1
     return prices_high
 
 
-def y_data_1(w_rate):
+last_backup = 0
+
+
+def y_data_low(w_rate):
     new_counter = 0
     prices_low = dates_numbers.copy()
-    last_backup = 0
-    for time_iterator in dates_numbers:
+    for time_iterator in dates_datetime:
         for backup_date_iterator in backups:
-            if time_iterator == backup_date_iterator:
-                last_backup = backup_date_iterator
-        prices_low[new_counter] = (dates_numbers[new_counter] - last_backup) * w_rate
+            if time_iterator == backup_date_iterator.date:
+                global last_backup
+                last_backup = backup_date_iterator.date
+        if last_backup != 0:
+            prices_low[new_counter] = (dates_datetime[new_counter] - last_backup).days * w_rate
+        else:
+            prices_low[new_counter] = dates_datetime[new_counter].day * w_rate
         new_counter += 1
     return prices_low
 
@@ -103,12 +109,12 @@ random_data = data_random(a, days - 4)
 fig, ax = plt.subplots()
 plt.subplots_adjust(left=0.15, bottom=0.3)
 
-prices = y_data(init_price_big, init_price_small, initial_work_rate)
+prices = y_data_high(init_price_big, init_price_small, initial_work_rate)
 l, = plt.plot(dates_datetime, prices, lw=1.2)
 plt.xlabel("Disaster date")
 plt.ylabel("Backup price")
 
-prices_1 = y_data_1(initial_work_rate)
+prices_1 = y_data_low(initial_work_rate)
 k, = plt.plot(dates_datetime, prices_1, color='red', lw=1.2)
 
 m = plt.scatter(random_data[0], random_data[1], color=random_data[2], lw=1.2)
@@ -133,8 +139,8 @@ def update(val):
     price_small = s_price_small.val
     work_rate = s_work_rate.val
     fig.canvas.draw_idle()
-    l.set_ydata(y_data(price_big, price_small, work_rate))
-    k.set_ydata(y_data_1(work_rate))
+    l.set_ydata(y_data_high(price_big, price_small, work_rate))
+    k.set_ydata(y_data_low(work_rate))
 
 
 s_work_rate.on_changed(update)

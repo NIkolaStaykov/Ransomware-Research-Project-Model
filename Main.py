85a98e911aa2ab5f0bc6431c280947d433e7d8bc
nikola_staykov <nikola@st>
2019-11-21 20:37:55 +0200

    Managed to get a graph from the math function. Still working on multiple graphs.

--- Main.py
+++ Main.py
@@ -1,20 +1,34 @@
-import Model as md
-# import numpy as np
+# import Model as md
+# # import numpy as np
+#
+#
+# sim = md.Simulation(200, 690, 130, 5, 0, 3)
+# str1 = input("Price: ")
+# ransom_value = int(str1)
+# str1 = input("Acceptable Error: ")
+# error_boundary = float(str1)
+# error_boundary = error_boundary/1000
+#
+# b = 1
+# while b:
+#     error_percentage = md.error_percent(sim, ransom_value)
+#     if error_percentage < error_boundary:
+#         b = 0
+#         print("Minimal sample size: ", sim.ppl_sample_size)
+#     sim.ppl_sample_size += 20
 
+import matplotlib.pyplot as plt
+import Mathematical_function_backups as math
 
-sim = md.Simulation(200, 690, 130, 5, 0, 3)
-str1 = input("Price: ")
-ransom_value = int(str1)
-str1 = input("Acceptable Error: ")
-error_boundary = float(str1)
-error_boundary = error_boundary/1000
-
-b = 1
-while b:
-    error_percentage = md.error_percent(sim, ransom_value)
-    if error_percentage < error_boundary:
-        b = 0
-        print("Minimal sample size: ", sim.ppl_sample_size)
-    sim.ppl_sample_size += 20
-
+math.setting_the_constants()
+x_data = []
+y_data = []
+for k in range(100):
+    x_data.append(k)
+    y_data.append(math.expected_price(k))
+print(x_data)
+print(y_data)
+plt.plot(x_data, y_data)
+name = 'Math_plot_' + str(k) + '.png'
+plt.savefig(name, bbox_inches='tight')
 
--- Mathematical_function_backups.py
+++ Mathematical_function_backups.py
@@ -1,8 +1,10 @@
 import numpy as np
+
+
 full = 0
 incremental = 0
-not_backed = int(input("Not backed: "))
-work_rate = int(input("Work rate: "))
+not_backed = 10  # int(input("Not backed: "))
+work_rate = 50  # int(input("Work rate: "))
 
 
 class Backup:
@@ -20,24 +22,35 @@ def setting_the_constants():
     incremental = Backup()
 
 
-def days_from_working_full(i, x):
-    number_of_fails = i
-    days_from_first = x
+def days_from_successful_full(number_of_fails, days_from_first):
     global full
-    days = full.interval(np.modf(days_from_first/full.interval)[0] + number_of_fails)
+    days = full.interval*(np.modf(days_from_first/full.interval)[0] + number_of_fails)
     return days
 
 
-def expected_price(x):
-    days_from_first_backup = x
+def incremental_count(days_from_success_full):
+    global full, incremental
+    if full.interval < days_from_success_full:
+        return np.modf(full.interval/incremental.interval)[1] - 1
+    else:
+        return np.modf(days_from_success_full/incremental.interval)[1]
+
+
+def incremental_cost(days_from_succ_full):
+    global incremental, full, work_rate
+    l = int(incremental_count(days_from_succ_full))
+    price = pow(1-incremental.probability, l)*((full.interval - incremental.interval*l)*work_rate + incremental.price*l)
+    for i in range(l):
+        price += pow(1-incremental.probability, i)*incremental.probability*((days_from_succ_full - incremental.interval * i) * work_rate + incremental.price * (i + 1))
+    return price
+
+
+def expected_price(days_from_first_backup):
+    x = days_from_first_backup
     global full, incremental, not_backed, work_rate
-    print(full.probability)
-    work_total = (days_from_first_backup + not_backed)*work_rate
-    full_backups_count = np.modf(days_from_first_backup/full.interval)[1] + 1
-    price = pow(full.probability, full_backups_count)(work_total + full_backups_count*full.price)
-    print(price)
-
-
-setting_the_constants()
-expected_price(30)
-
+    work_total = (x + not_backed)*work_rate
+    full_backups_count = np.modf(x/full.interval)[1] + 1
+    price = pow(full.probability, full_backups_count)*(work_total + full_backups_count*full.price)
+    for i in range(int(full_backups_count)):
+        price += (1-full.probability)*pow(full.probability, i)*(incremental_cost(days_from_successful_full(i, x)) + i * full.price)
+    return price

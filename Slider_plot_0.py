35bb951e1e74386ceb6c19f4d9418959babb7b41
nikola_staykov <nikola@st>
2019-11-23 00:54:54 +0200

    Regular commit

--- Slider_plot_0.py
+++ Slider_plot_0.py
@@ -102,7 +102,7 @@ def data_random(point_generator, points_count):
 
 
 disaster_date = backups[-1].date.strftime('%m/%d/%Y')
-a = Backup_cost.Backup_cost(initial_work_rate, init_price_small, init_price_big, disaster_date, backups, 0.5, 1/3)
+a = Backup_cost.Backup_cost(initial_work_rate, init_price_small, init_price_big, disaster_date, backups, 0.19, 1/3)
 random_data = data_random(a, days-4)
 
 # Plotting the data
